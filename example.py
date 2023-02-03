101f2b1b4ad0336e1be8da83f40e5dd4674f7c0d
Benjamín Miranda <supremobenjamin.cia@gmail.com>
2023-02-03 01:50:32 -0300

    first commit

---
+++ example.py
@@ -0,0 +1,10 @@
+import vectorial_function
+
+function = vectorial_function.new_function_vector_r3("sin(t)", "cos(t)", "t", "t")
+
+function.derive()
+function.derive()
+
+function.show_function()
+function.show_function_derivative(1)
+function.show_function_derivative(2)
---
+++ vectorial_function.py
@@ -0,0 +1,115 @@
+from sympy import *
+
+WITHOUT_DIMENSION = "without_dimension"
+R_2 = "R_2"
+R_3 = "R_3"
+
+
+class VectorialFunction:
+    derives = []
+
+    def __init__(self,
+                 function_f,
+                 function_g,
+                 function_h,
+                 variable):
+        self.function_f = function_f
+        self.function_g = function_g
+        self.function_h = function_h
+
+        if self.function_h == "":
+            self.function_h = WITHOUT_DIMENSION
+
+        self.variable = variable
+
+    def show_function(self):
+
+        if self.get_dimension() == R_2:
+            print("r(t) = {f}i + {g}j".format(
+                f=self.function_f,
+                g=self.function_g,
+            ))
+            return
+
+        print("r(t) = {f}i + {g}j + {h}k".format(
+            f=self.function_f,
+            g=self.function_g,
+            h=self.function_h
+        ))
+
+    def show_function_derivative(self, order):
+        derivative = self.get_derivatives(order)
+
+        derivative_symbol = "'" * order
+
+        if self.get_dimension() == R_2:
+            print("r(t)" + derivative_symbol + " = {f}i + {g}j".format(
+                f=derivative[0],
+                g=derivative[1],
+            ))
+            return
+
+        print("r(t)" + derivative_symbol + " = {f}i + {g}j + {h}k".format(
+            f=derivative[0],
+            g=derivative[1],
+            h=derivative[2]
+        ))
+
+    def get_derivatives(self, order):
+        order = order - 1
+
+        if order < 0:
+            return self.get_derivatives(1)
+
+        return self.derives[order]
+
+    def get_dimension(self):
+        if self.function_h == WITHOUT_DIMENSION:
+            return R_2
+        return R_3
+
+    def get_derivative_in_top(self):
+        derive = []
+
+        if len(self.derives) == 0:
+            derive.append(self.function_f)
+            derive.append(self.function_g)
+
+            if self.get_dimension() == R_3:
+                derive.append(self.function_h)
+
+            return derive
+
+        return self.derives[len(self.derives) - 1]
+
+    def derive(self):
+        derive = []
+
+        derivative_top = self.get_derivative_in_top()
+
+        function_f_derivative = diff(derivative_top[0], self.variable)
+        function_g_derivative = diff(derivative_top[1], self.variable)
+
+        derive.append(
+            function_f_derivative
+        )
+        derive.append(
+            function_g_derivative
+        )
+
+        if self.get_dimension() == R_3:
+            function_h_derivative = diff(derivative_top[2], self.variable)
+            derive.append(
+                function_h_derivative
+            )
+
+        self.derives.append(derive)
+
+
+def new_function_vector_r3(function_f, function_g, function_h, variable):
+    return VectorialFunction(function_f, function_g, function_h, variable)
+
+
+def new_function_vector_r2(function_f, function_g, variable):
+    return VectorialFunction(function_f, function_g, WITHOUT_DIMENSION, variable)
+
