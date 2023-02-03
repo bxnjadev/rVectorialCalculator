258b61f7d8424863fa0a8d7cfd6f83e9d362b930
Benjamín Miranda <supremobenjamin.cia@gmail.com>
2023-02-03 11:21:32 -0300

    updated

--- example.py
+++ example.py
@@ -8,10 +8,6 @@ function = vectorial_function.new_function_vector_r3("sin(t)", "cos(t)", "t", "t
 function.derive()
 function.derive()
 
-function.show_function()
-function.show_function_derivative(1)
-function.show_function_derivative(2)
-
 print(
     function.get_magnitude()
 )
